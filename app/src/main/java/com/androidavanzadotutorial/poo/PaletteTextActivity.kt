0222f3c6d5eb58e11f6c6b44556287a47abafb36
Juan J Salgado L <zallagox95@hotmail.com>
2022-11-16 15:43:19 -0500

    feat 🚀 : TextView desde xml

---
+++ app/src/main/java/com/androidavanzadotutorial/poo/PaletteTextActivity.kt
@@ -0,0 +1,11 @@
+package com.androidavanzadotutorial.poo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PaletteTextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_palette_text)
+    }
+}
