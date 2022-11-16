ed4de0a5d0adad397862ba855cfcd55486b2c141
Juan J Salgado L <zallagox95@hotmail.com>
2022-11-16 15:54:11 -0500

    feat 🚀 : TextView desde código

--- app/src/main/java/com/androidavanzadotutorial/poo/PaletteTextActivity.kt
+++ app/src/main/java/com/androidavanzadotutorial/poo/PaletteTextActivity.kt
@@ -1,11 +1,19 @@
 package com.androidavanzadotutorial.poo
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class PaletteTextActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_palette_text)
+
+        var tvEjemplo:TextView = findViewById(R.id.tvEjemplo)
+        tvEjemplo.text = "Texto cambiado desde codigo"
+        tvEjemplo.setTextColor(Color.RED)
+        tvEjemplo.setTypeface(Typeface.MONOSPACE,Typeface.BOLD)
     }
 }
