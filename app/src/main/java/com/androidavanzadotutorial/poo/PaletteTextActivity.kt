1c4addb15730916b9e52c252c1c870ea9ef8cc6d
Juan J Salgado <zallagox95@hotmail.com>
2022-11-19 16:27:57 -0500

    Feat 🚀 : MultiAutoCompleteTextView

--- app/src/main/java/com/androidavanzadotutorial/poo/PaletteTextActivity.kt
+++ app/src/main/java/com/androidavanzadotutorial/poo/PaletteTextActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.EditText
+import android.widget.MultiAutoCompleteTextView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
@@ -38,11 +39,33 @@ class PaletteTextActivity : AppCompatActivity() {
         var fin = etEjemplo.selectionEnd
         etEjemplo.selectAll()
 
+        /*
+            tipos de inputType
+
+            text 	            Recibe texto plano simple
+            textPersonName 	    Texto correspondiente al nombre de una persona
+            textPassword 	    Protege los caracteres que se van escribiendo con puntos
+            numberPassword 	    Contraseña de solo números enmascarada con puntos
+            textEmailAddress 	Texto que será usado en un campo para emails
+            phone 	            Texto asociado a un número de teléfono
+            textPostalAddress 	Para ingresar textos asociados a una dirección postal
+            textMultiLine 	    Permite múltiples líneas en el campo de texto
+            time 	            Texto para determinar la hora
+            date 	            Texto para determinar la fecha
+            number 	            Texto con caracteres numéricos
+            numberSigned 	    Permite números con signo
+            numberDecimal 	    Para ingresar números decimales
+         */
+
         var autoCompleteTextView: AutoCompleteTextView = findViewById(R.id.autoCompleteTextView)
         var countries: Array<String> = resources.getStringArray(R.array.countries_array)
         var adapter: ArrayAdapter<String> = ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,countries)
 
         autoCompleteTextView.setAdapter(adapter)
+
+        var multiAutoCompleteTextView: MultiAutoCompleteTextView = findViewById(R.id.multiAutoCompleteTextView)
+        multiAutoCompleteTextView.setAdapter(adapter)
+        multiAutoCompleteTextView.setTokenizer(MultiAutoCompleteTextView.CommaTokenizer())
 
 
     }
