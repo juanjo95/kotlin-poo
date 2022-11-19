e1241c91d0522fb446c3a2146385e647b6a9bd56
Juan J Salgado <zallagox95@hotmail.com>
2022-11-19 17:12:18 -0500

    Feat 🚀 : Button

---
+++ app/src/main/java/com/androidavanzadotutorial/poo/PaletteButtonActivity.kt
@@ -0,0 +1,11 @@
+package com.androidavanzadotutorial.poo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PaletteButtonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_palette_button)
+    }
+}
