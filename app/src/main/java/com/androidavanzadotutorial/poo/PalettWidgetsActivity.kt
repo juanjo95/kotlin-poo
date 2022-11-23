3da2b99a4c7641c4820c5fa5a4eea8e77fe52c7b
Juan J Salgado L <zallagox95@hotmail.com>
2022-11-23 17:15:08 -0500

    feat 🚀 : ImageView externa URL (Widgets)

--- app/src/main/java/com/androidavanzadotutorial/poo/PalettWidgetsActivity.kt
+++ app/src/main/java/com/androidavanzadotutorial/poo/PalettWidgetsActivity.kt
@@ -2,10 +2,20 @@ package com.androidavanzadotutorial.poo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 
 class PalettWidgetsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_palett_widgets)
+
+        /**
+         * ImageView Url
+         */
+        var ivEjemplo: ImageView = findViewById(R.id.iv)
+        val imageURL = "https://http2.mlstatic.com/D_NQ_NP_669593-MCO50195190532_062022-O.webp"
+        Picasso.get().load(imageURL).into(ivEjemplo)
+
     }
 }
