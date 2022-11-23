26bca2b8fc0507dc00c6349e23b604d971a7a328
Juan J Salgado L <zallagox95@hotmail.com>
2022-11-23 16:46:14 -0500

    feat 🚀 : ImageView (Widgets)

---
+++ app/src/main/java/com/androidavanzadotutorial/poo/PalettWidgetsActivity.kt
@@ -0,0 +1,11 @@
+package com.androidavanzadotutorial.poo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PalettWidgetsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_palett_widgets)
+    }
+}
