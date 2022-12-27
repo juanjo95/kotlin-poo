e783d7a900e33e0ed8e0a04f24b0bd9693bc42c1
Juan J Salgado L <zallagox95@hotmail.com>
2022-12-27 09:12:32 -0500

    feat 🚀 : Unidades de Medida

---
+++ app/src/main/java/com/androidavanzadotutorial/poo/UnidadesMedidaActivity.kt
@@ -0,0 +1,11 @@
+package com.androidavanzadotutorial.poo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UnidadesMedidaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unidades_medida)
+    }
+}
