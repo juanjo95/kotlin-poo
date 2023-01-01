d7d68fb87f7bb06a2f0d0582558d29c6f1a7f836
Juan J Salgado <zallagox95@hotmail.com>
2023-01-01 12:24:40 -0500

    Feat 🚀 : Aplicación calculadora con GridLayout

---
+++ app/src/main/java/com/androidavanzadotutorial/poo/CalculadoraGridActivity.kt
@@ -0,0 +1,11 @@
+package com.androidavanzadotutorial.poo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalculadoraGridActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculadora_grid)
+    }
+}
